ecbf30d73c3a03ea7657121045adcbc1c668f41a
VeressIris <irisveress@gmail.com>
2023-07-28 20:35:12 +0300

    player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Ground Check")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpForce = 16f;
+    private float horizontal;
+
+    private bool facingRight = true;
+
     void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+
+        if (!facingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+        else if (facingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (ctx.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        horizontal = ctx.ReadValue<Vector2>().x;
     }
 }
